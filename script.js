00bc6a1a0f5e5a524dbce7f57f7ad2b5ee09facb
Christy Mugomba <108655633+christymugs@users.noreply.github.com>
2024-01-12 17:08:48 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,45 +1,49 @@
-//DOM
+// JavaScript
+
 const swiper = document.querySelector('#swiper');
 const network = document.querySelector('#network');
 const no = document.querySelector('#no');
-// constants
 
-const urls = [
-    'https://source.unsplash.com/random/1000x1000?sky',
-    'https://source.unsplash.com/random/1000x1000?landsacpe',
-    'https://source.unsplash.com/random/1000x1000?ocean',
-    'https://source.unsplash.com/random/1000x1000?mountain',
-    'https://source.unsplash.com/random/1000x1000?forest'
-]
+// Local paths to your images
+const localImagePaths = [
+    '1.jpg',
+    '2.jpg',
+    '3.jpg',
+    '4.jpg',
+    '5.jpg'
+];
 
-//varibales
+// Constants
+const urls = localImagePaths.map(path => `file://${path}`);
 
+// Variables
 let cardCount = 0;
 
-function appendNewCard(){
+function appendNewCard() {
     const card = new Card({
         imageUrl: urls[cardCount % 5],
         onDismiss: appendNewCard,
-        onNetwork:() =>{
+        onNetwork: () => {
             network.style.animationPlayState = 'running';
             network.classList.toggle('trigger');
         },
-        onNo:()=>{
+        onNo: () => {
             no.style.animationPlayState = 'running';
             no.classList.toggle('trigger');
         }
     });
-    //card.element.style.setProperty('--i',cardCount%5)
+
     swiper.append(card.element);
-    cardCount++
+    cardCount++;
 
     const cards = swiper.querySelectorAll('.card:not(.dismissing)');
     cards.forEach((card, index) => {
         card.style.setProperty('--i', index);
-    });    
+    });
 }
 
-// first 5
-for(let i = 0; i < 5; i++) {
+// First 5
+for (let i = 0; i < 5; i++) {
     appendNewCard();
 }
+
